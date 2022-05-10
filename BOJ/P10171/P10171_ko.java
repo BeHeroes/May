d97e0579436fee6f2fb1e998bc777c4cca5bb95d
naryeong-ko <99406992+naryeong-ko@users.noreply.github.com>
2022-05-11 00:33:29 +0900

    220511 : P10171_ko.java

---
+++ BOJ/P10171/P10171_ko.java
@@ -0,0 +1,11 @@
+public class P10171 {
+
+	public static void main(String[] args) {
+		String str = "\\    /\\\n" + 
+					" )  ( ')\n" + 
+					"(  /  )\n" + 
+					" \\(__)|";
+		System.out.print(str);
+	}
+
+}
