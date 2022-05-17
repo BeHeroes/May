ed6ae6d4345b63c1722c5979cd43983ef6797dc4
Moon-hongwoong <104899234+Moon-hongwoong@users.noreply.github.com>
2022-05-17 19:52:12 +0900

    220517 : P10952_moon

---
+++ BOJ/P10952/P10952_moon.c
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B;
+    while(1){
+        scanf("%d%d", &A, &B);
+        if( A==0 && B==0) break;
+        printf("%d\n", A+B);
+    }
+}
